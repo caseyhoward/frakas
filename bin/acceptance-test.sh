fdc829a31b559d3a32ad045d5731b376c80e72a5
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-23 17:33:05 -0800

    Fix aws keys

--- bin/acceptance-test.sh
+++ bin/acceptance-test.sh
@@ -6,6 +6,8 @@ npm install -g serverless@1.58.0
 
 cd serverless
 serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV}
 FRACAS_WEBSOCKET_ENDPOINT=`serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2`
 echo "FRACAS_WEBSOCKET_ENDPOINT=$FRACAS_WEBSOCKET_ENDPOINT"
 cd ../acceptance-tests
