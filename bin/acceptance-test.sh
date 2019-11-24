cf1d44d14bc0d51ebbcb87cc2e24192cb29a31a5
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-23 17:11:09 -0800

    Trying to fix error

--- bin/acceptance-test.sh
+++ bin/acceptance-test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 set -e
-set -x
 
 npm install -g serverless@1.58.0
 
 cd serverless
 FRACAS_WEBSOCKET_ENDPOINT=`serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2`
+echo "FRACAS_WEBSOCKET_ENDPOINT=$FRACAS_WEBSOCKET_ENDPOINT"
 cd ../acceptance-tests
 FRACAS_WEBSOCKET_ENDPOINT=$FRACAS_WEBSOCKET_ENDPOINT bin/test.sh
