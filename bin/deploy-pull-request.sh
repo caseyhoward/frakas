57387c4aef901f9fa7ddca6e895cb9d8567cf126
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-24 09:31:15 -0800

    Refactor

--- bin/deploy-pull-request.sh
+++ bin/deploy-pull-request.sh
@@ -7,6 +7,6 @@ serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpoi
 export FRACAS_WEBSOCKET_ENDPOINT=$(cat FRACAS_WEBSOCKET_ENDPOINT.txt)
  
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": \"Deployed version ${TRAVIS_PULL_REQUEST_SHA}.\nWebsocketUrl: ${FRACAS_WEBSOCKET_ENDPOINT}\"}" \
+-d "{\"body\": \"Deployed ${TRAVIS_PULL_REQUEST_SHA}\nWebsocketUrl: ${FRACAS_WEBSOCKET_ENDPOINT}\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 cd ..
