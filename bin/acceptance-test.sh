f6a8d51c5fdf2141e5cc569196726cdaa244fe1a
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-23 19:29:22 -0800

    Revert "Add pr comment with websocket url"
    This reverts commit 4c54b5b77c0cab76970f0d0663d2f67ce02f6d3b.

--- bin/acceptance-test.sh
+++ bin/acceptance-test.sh
@@ -7,9 +7,10 @@ npm install -g serverless@1.58.0
 cd serverless
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV}
+serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST}
 serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2 > FRACAS_WEBSOCKET_ENDPOINT.txt
 export FRACAS_WEBSOCKET_ENDPOINT=$(cat FRACAS_WEBSOCKET_ENDPOINT.txt)
 echo "FRACAS_WEBSOCKET_ENDPOINT=$FRACAS_WEBSOCKET_ENDPOINT"
 cd ../acceptance-tests
 FRACAS_WEBSOCKET_ENDPOINT=$FRACAS_WEBSOCKET_ENDPOINT bin/test.sh
-cd ..
+rm FRACAS_WEBSOCKET_ENDPOINT.txt
--- serverless/bin/deploy-pull-request.sh
+++ serverless/bin/deploy-pull-request.sh
@@ -8,9 +8,4 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV}
 export FRACAS_TABLE_NAME_SUFFIX=_PR_${TRAVIS_PULL_REQUEST}
 serverless deploy --stage pr-${TRAVIS_PULL_REQUEST}
-serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2 > FRACAS_WEBSOCKET_ENDPOINT.txt
-export FRACAS_WEBSOCKET_ENDPOINT=$(cat FRACAS_WEBSOCKET_ENDPOINT.txt)
 
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": \"Deployed version ${TRAVIS_PULL_REQUEST_SHA}.\nWebsocketUrl: ${FRACAS_WEBSOCKET_ENDPOINT}\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
