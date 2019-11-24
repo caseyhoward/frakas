9a8648ac289f991f74e43bf007cbd301d21abe5e
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-24 09:41:18 -0800

    Attempt to fix github commenting

--- bin/deploy-pull-request.sh
+++ bin/deploy-pull-request.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV}
+
 cd serverless && bin/deploy-pull-request.sh && cd ..
 
 cd serverless
--- serverless/bin/deploy-pull-request.sh
+++ serverless/bin/deploy-pull-request.sh
@@ -4,8 +4,6 @@ set -e
 set -x
 
 npm install -g serverless@1.58.0
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV}
 export FRACAS_TABLE_NAME_SUFFIX=_PR_${TRAVIS_PULL_REQUEST}
 serverless deploy --stage pr-${TRAVIS_PULL_REQUEST}
 
