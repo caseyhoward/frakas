ae7ae5770a12fd365be4fbf850059a4018e9bac5
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-24 10:56:54 -0800

    Refactor to use resolvers from old project

--- core/src/Resolvers.ts
+++ core/src/Resolvers.ts
@@ -19,6 +19,58 @@ import { updateMapForGame } from "./resolvers/Mutation/updateGameMap";
 import * as SubscriptionGame from "./resolvers/Subscription/game";
 import * as SubscriptionGameOrConfiguration from "./resolvers/Subscription/gameOrConfiguration";
 import * as PubSub from "./PubSub";
+import { ulid } from "ulid";
+
+import { withFilter } from "aws-lambda-graphql";
+
+// import * as PubSub from "fracas-core/src/PubSub";
+
+type MessageType = "greeting" | "test";
+
+type Message = {
+  id: string;
+  text: string;
+  type: MessageType;
+};
+
+type SendMessageArgs = {
+  text: string;
+  type: MessageType;
+};
+
+// const environment: Environment.Environment = Environment.create();
+
+// export function create(
+//   // repository: Repository.Repository,
+//   pubSub: PubSub.PubSub
+// ): IResolvers<any, any> {
+//   return {
+//     Mutation: {
+//       async sendMessage(rootValue: any, { text, type }: SendMessageArgs) {
+//         const payload: Message = { id: ulid(), text, type };
+//         await pubSub.publish("NEW_MESSAGE", payload);
+//         return payload;
+//       }
+//     },
+//     Subscription: {
+//       messageFeed: {
+//         resolve: (rootValue: Message) => {
+//           // root value is the payload from sendMessage mutation
+//           return rootValue;
+//         },
+//         subscribe: withFilter(
+//           pubSub.subscribe("NEW_MESSAGE"),
+//           (rootValue: Message, args: { type: null | MessageType }) => {
+//             if (args.type == null) {
+//               return true;
+//             }
+//             return args.type === rootValue.type;
+//           }
+//         )
+//       }
+//     }
+//   };
+// }
 
 export function create(
   repository: Repository.Repository,
@@ -67,6 +119,21 @@ export function create(
       test: {
         subscribe: PubSub.subscribeTest(pubsub),
         resolve: () => true
+      },
+      messageFeed: {
+        resolve: (rootValue: Message) => {
+          // root value is the payload from sendMessage mutation
+          return rootValue;
+        },
+        subscribe: withFilter(
+          pubsub.subscribe("NEW_MESSAGE"),
+          (rootValue: Message, args: { type: null | MessageType }) => {
+            if (args.type == null) {
+              return true;
+            }
+            return args.type === rootValue.type;
+          }
+        )
       }
     }
   };
@@ -101,6 +168,11 @@ export function createWithoutSubscriptions(
       }
     },
     Mutation: {
+      async sendMessage(rootValue: any, { text, type }: SendMessageArgs) {
+        const payload: Message = { id: ulid(), text, type };
+        await pubsub.publish("NEW_MESSAGE", payload);
+        return payload;
+      },
       createMap: async (_, input) => createMap(repository.createMap, input),
       joinGame: (_, input) =>
         joinGame(
