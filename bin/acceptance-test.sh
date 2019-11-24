fe6ebe67c7c6089459684f7c16f1214a575dc28c
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-23 16:52:41 -0800

    Try to fix missing enviroment variable

--- bin/acceptance-test.sh
+++ bin/acceptance-test.sh
@@ -6,6 +6,6 @@ set -x
 # npm install -g serverless@1.58.0
 
 cd serverless
-export FRACAS_WEBSOCKET_ENDPOINT=`serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2`
+FRACAS_WEBSOCKET_ENDPOINT=`serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2`
 cd ../acceptance-tests
-bin/test.sh
+FRACAS_WEBSOCKET_ENDPOINT=$FRACAS_WEBSOCKET_ENDPOINT bin/test.sh
