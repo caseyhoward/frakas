855db7a16ae12340d038d7ac1e22162dd55d625b
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-25 11:55:48 -0800

    Don't deploy core dev dependencies

--- bin/deploy-pull-request.sh
+++ bin/deploy-pull-request.sh
@@ -6,6 +6,7 @@ set -e
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV}
 
+cd core && rm -rf node_modules && npm install --production
 cd serverless && bin/deploy-pull-request.sh && cd ..
 
 cd serverless
