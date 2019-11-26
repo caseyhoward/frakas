51305bdb765ec72e51169903c2f124593135fffe
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-25 21:57:30 -0800

    Fix things

--- serverless/bin/deploy-pull-request.sh
+++ serverless/bin/deploy-pull-request.sh
@@ -6,6 +6,7 @@ set -x
 npm install -g serverless@1.58.0
 export FRACAS_TABLE_NAME_SUFFIX=_PR_${TRAVIS_PULL_REQUEST}
 export BUCKET_NAME=fracas-client-pr-${TRAVIS_PULL_REQUEST}
+export FRACAS_CLIENT_DOMAIN=http://example.com
 
 # TODO: This only works after the first deployment
 serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ^WebAppCloudFrontDistributionOutput: d| cut -d ' ' -f 2 > CLOUDFRONT_DOMAIN.txt
