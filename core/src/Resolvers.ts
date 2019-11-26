349209aec2dff5ef0e28014fe92614c32073630b
Casey Howard <caseyhoward@caseyhoward.com>
2019-11-25 17:02:20 -0800

    Fix bucket name

--- core/src/Resolvers.ts
+++ core/src/Resolvers.ts
@@ -38,40 +38,6 @@ type SendMessageArgs = {
   type: MessageType;
 };
 
-// const environment: Environment.Environment = Environment.create();
-
-// export function create(
-//   // repository: Repository.Repository,
-//   pubSub: PubSub.PubSub
-// ): IResolvers<any, any> {
-//   return {
-//     Mutation: {
-//       async sendMessage(rootValue: any, { text, type }: SendMessageArgs) {
-//         const payload: Message = { id: ulid(), text, type };
-//         await pubSub.publish("NEW_MESSAGE", payload);
-//         return payload;
-//       }
-//     },
-//     Subscription: {
-//       messageFeed: {
-//         resolve: (rootValue: Message) => {
-//           // root value is the payload from sendMessage mutation
-//           return rootValue;
-//         },
-//         subscribe: withFilter(
-//           pubSub.subscribe("NEW_MESSAGE"),
-//           (rootValue: Message, args: { type: null | MessageType }) => {
-//             if (args.type == null) {
-//               return true;
-//             }
-//             return args.type === rootValue.type;
-//           }
-//         )
-//       }
-//     }
-//   };
-// }
-
 export function create(
   repository: Repository.Repository,
   pubsub: PubSub.PubSub
@@ -116,10 +82,6 @@ export function create(
         },
         subscribe: PubSub.subscribeGame(pubsub)
       },
-      // test: {
-      //   subscribe: PubSub.subscribeTest(pubsub),
-      //   resolve: () => true
-      // },
       messageFeed: {
         resolve: (rootValue: Message) => {
           // root value is the payload from sendMessage mutation
--- serverless/bin/deploy-pull-request.sh
+++ serverless/bin/deploy-pull-request.sh
@@ -5,6 +5,7 @@ set -x
 
 npm install -g serverless@1.58.0
 export FRACAS_TABLE_NAME_SUFFIX=_PR_${TRAVIS_PULL_REQUEST}
+export BUCKET_NAME=fracas-client-pr-${TRAVIS_PULL_REQUEST}
 serverless deploy --stage pr-${TRAVIS_PULL_REQUEST}
 
 serverless info --verbose --stage pr-${TRAVIS_PULL_REQUEST} | grep ServiceEndpointWebsocket | cut -d ' ' -f 2 > FRACAS_WEBSOCKET_ENDPOINT.txt
